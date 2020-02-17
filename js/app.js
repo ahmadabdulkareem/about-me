579f12bab751f460fdd7bae8d606bc20ef7dee8d
ahmad <auepior@gmail.com>
2020-02-17 21:29:51 +0200

    adding defaults

--- js/app.js
+++ js/app.js
@@ -2,38 +2,34 @@
 'use strict';
 
 var name = prompt('what is your name?');
-console.log('hello ' + name);
 alert('Hello ' + name);
 
 var myName = prompt('Do you know my name?(Yes/no)');
-console.log(myName);
-
 switch (myName.toLowerCase()) {
     case 'yes':
     case 'y':
-        console.log("yeah, it's Ahmad ");
         alert("yeah, it's Ahmad ");
         break;
     case 'no':
     case 'n':
-        console.log("well, it's Ahmad Nice to meet you ", name);
         alert("well, it's Ahmad Nice to meet you ", name);
+        default:
+            alert("Answer Only Yes or no");
 }
 
 
 var paris = prompt('Have you ever been to paris?!');
-console.log(paris);
 
 switch (paris.toLowerCase()) {
     case 'yes':
     case 'y':
-        console.log("luckly I havn't. ");
         alert("luckly I havn't.");
         break;
     case 'no':
     case 'n':
-        console.log("Rihgt, Obviously(0_0)");
         alert("Rihgt, Obviously(0_0)");
+        default:
+            alert("Answer Only Yes or no");
 }
 
 var pop = prompt("do you like pop music?!");
@@ -46,6 +42,8 @@ switch (pop.toLowerCase()) {
     case 'no':
     case 'n':
         alert("Wrong, I do like it.");
+        default:
+            alert("Answer Only Yes or no");
 }
 
 var nancy = prompt("Do you love Nancy Ajram? ha!");
@@ -53,24 +51,24 @@ var nancy = prompt("Do you love Nancy Ajram? ha!");
 switch (nancy.toLowerCase()) {
     case 'yes':
     case 'y':
-        console.log(" yupppy, who dosent!. ");
         alert("yupppy, who doesn't");
         break;
     case 'no':
     case 'n':
-        console.log("oopsy.not righty!");
         alert("oopsy.not righty!");
+        default:
+            alert("Answer Only Yes or no");
 }
 
 var goal = prompt("Is one of your goals is to get new Stockings?");
 switch (nancy.toLowerCase()) {
     case 'yes':
     case 'y':
-        console.log(" what a dumb question is that of course no ");
         alert("what a dumb question is that, of course no!!!");
         break;
     case 'no':
     case 'n':
-        console.log(" hhhh- right!");
         alert("hhhh- right!");
+        default:
+            alert("Answer Only Yes or no");
 }
